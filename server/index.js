1914bff9438d20cd30c0dc05c9ed3eb110a0cc92
chaudhuree <chaudhuree00@gmail.com>
2023-07-08 21:43:23 +0600

    bs: backend necessary code for payment in the post method

--- server/index.js
+++ server/index.js
@@ -20,8 +20,47 @@ app.get("/", (req, res) => {
 });
 
 //checkout route
-app.post("/checkout", (req, res) => {
-  console.log("request:", req.body);
+app.post("/checkout", async (req, res) => {
+  // console.log("request:", req.body);
+  let error, status;
+  try {
+    const { product, token } = req.body;
+    // console.log("product:", product);
+    // console.log("price:", product.price);
+    // console.log("token:", token);
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+    // console.log("customer:", customer);
+    const key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.price * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `purchase of ${product.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip,
+          },
+        },
+      },
+      { idempotencyKey: key }
+    );
+    console.log("charge:", charge);
+    status = "success";
+  } catch (error) {
+    console.log("error:", error);
+    status = "failure";
+  }
+  res.json({ error, status });
 });
 
 //server listening
