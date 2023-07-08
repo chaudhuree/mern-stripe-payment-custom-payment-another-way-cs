eae7e9c4012843caa8932f4ab29ddc6456924361
chaudhuree <chaudhuree00@gmail.com>
2023-07-08 21:00:19 +0600

    cs: added bootstrap cdn and imported stripe button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
   return (
     <>
       <Toaster />
       <h1>stripe payment</h1>
+      <StripeCheckout/>
     </>
   );
 }
