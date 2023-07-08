1867688fb6349caccbd3e68bdc1b6fcd22c5f290
chaudhuree <chaudhuree00@gmail.com>
2023-07-08 21:22:53 +0600

    cs: necessary basic work done  now going to create backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import { Toaster } from "react-hot-toast";
+import toast,{ Toaster } from "react-hot-toast";
 import StripeCheckout from "react-stripe-checkout";
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  // product object for amount
+  const [product, setProduct] = useState({
+    name: "sample pruduct",
+    price: 10,
+    description: "This is a product discription"
+  });
+
+  // handle token
+  const handleToken = async (token,addresses) => {
+    const response = await axios.post("http://localhost:5000/checkout",{token,product})
+    // take response from server
+   console.log('response',response.status);
+   
+  }
   return (
     <>
       <Toaster />
@@ -12,7 +29,13 @@ function App() {
             <h1>stripe payment</h1>
           </div>
           <div className="col-md-12">
-            <StripeCheckout />
+            <StripeCheckout stripeKey="pk_test_51NRJ7VBhBYaeHe6cSOm8ypnr9Slz7M1sJH07ex9FLDVaKAS6XM8ZZhzOBN3ee1qqOJ8uTL9NMpLFbpmB0E5YEBwR00g8gMSGV5"
+            token={handleToken}
+            amount={product.price*100}
+            name={product.name}
+            billingAddress
+            shippingAddress
+            />
           </div>
         </div>
       </div>
