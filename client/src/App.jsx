6ca514b7e8617b6e7f614f2ad75207367772b3fc
chaudhuree <chaudhuree00@gmail.com>
2023-07-08 20:57:16 +0600

    cs: installed necessary packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-
-import './App.css'
+import { Toaster } from "react-hot-toast";
+import "./App.css";
 
 function App() {
   return (
     <>
-     
+      <Toaster />
+      <h1>stripe payment</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
