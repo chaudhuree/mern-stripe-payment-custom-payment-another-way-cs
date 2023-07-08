b2cb11b0ba3f698e68b5d205637ce58a95c950aa
chaudhuree <chaudhuree00@gmail.com>
2023-07-08 21:48:41 +0600

    cs:payment success toast setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,31 @@
-import toast,{ Toaster } from "react-hot-toast";
+import axios from "axios";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import StripeCheckout from "react-stripe-checkout";
 import "./App.css";
-import { useState } from "react";
-import axios from "axios";
 
 function App() {
-
   // product object for amount
   const [product, setProduct] = useState({
     name: "sample pruduct",
     price: 10,
-    description: "This is a product discription"
+    description: "This is a product discription",
   });
 
   // handle token
-  const handleToken = async (token,addresses) => {
-    const response = await axios.post("http://localhost:5000/checkout",{token,product})
+  const handleToken = async (token, addresses) => {
+    const response = await axios.post("http://localhost:5000/checkout", {
+      token,
+      product,
+    });
     // take response from server
-   console.log('response',response.status);
-   
-  }
+    console.log("response", response.status);
+    if (response.status === 200) {
+      toast.success("payment successfull");
+    } else {
+      toast.error("payment failed");
+    }
+  };
   return (
     <>
       <Toaster />
@@ -29,12 +35,13 @@ function App() {
             <h1>stripe payment</h1>
           </div>
           <div className="col-md-12">
-            <StripeCheckout stripeKey="pk_test_51NRJ7VBhBYaeHe6cSOm8ypnr9Slz7M1sJH07ex9FLDVaKAS6XM8ZZhzOBN3ee1qqOJ8uTL9NMpLFbpmB0E5YEBwR00g8gMSGV5"
-            token={handleToken}
-            amount={product.price*100}
-            name={product.name}
-            billingAddress
-            shippingAddress
+            <StripeCheckout
+              stripeKey="pk_test_51NRJ7VBhBYaeHe6cSOm8ypnr9Slz7M1sJH07ex9FLDVaKAS6XM8ZZhzOBN3ee1qqOJ8uTL9NMpLFbpmB0E5YEBwR00g8gMSGV5"
+              token={handleToken}
+              amount={product.price * 100}
+              name={product.name}
+              billingAddress
+              shippingAddress
             />
           </div>
         </div>
