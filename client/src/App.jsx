c0f733f967a46ab28667944dec4b7911f3adf46b
chaudhuree <chaudhuree00@gmail.com>
2023-07-08 21:15:39 +0600

    cs:added some design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import { Toaster } from "react-hot-toast";
-import "./App.css";
 import StripeCheckout from "react-stripe-checkout";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Toaster />
-      <h1>stripe payment</h1>
-      <StripeCheckout/>
+      <div className=" main-box container d-flex justify-content-center h-100 text-center align-items-center">
+        <div className="row ">
+          <div className="col-md-12 my-5">
+            <h1>stripe payment</h1>
+          </div>
+          <div className="col-md-12">
+            <StripeCheckout />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
